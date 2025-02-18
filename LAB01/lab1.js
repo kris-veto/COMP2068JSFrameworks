ee0864cb45e27c6b822d6ea8eb4f2d228d5e2b5e
Crhistian Vera Torres <elveraco001@hotmail.com>
2025-02-17 22:35:55 -0500

    Autor: Crhistian Vera 200557901 JavaScript Frameworks Lab 2

--- LAB01/lab1.js
+++ LAB01/lab1.js
@@ -1,1 +1,50 @@
-console.log("Hello world!");
+//Autor: Crhistian Vera 200557901
+//JavaScript Frameworks Lab 2
+
+const prompt = require('prompt');
+
+prompt.start();
+
+console.log("Enter your choice (ROCK, PAPER, OR SCISSORS): ");
+
+prompt.get(['userChoice'], function (err, result){
+    if (err) {
+        console.log("imput error."  );
+        return;
+
+    }
+    let userChoice = result.userChoice.toUpperCase();
+
+    // Check valid option
+    if (userChoice !== "ROCK" && userChoice !== "PAPER" && userChoice !== "SCISSORS"){
+        console.log("invalid choice! Please enter ROCK, PAPER, OR SCISSORS");
+        return;
+    }
+
+    //Generate a random  chice for th CP
+    let randomNumber = Math.random();
+    let computerChoice;
+
+    if (randomNumber < 0.34){
+        computerChoice = "ROCK";
+    }else if (randomNumber <0.67){
+        computerChoice = "PAPER";
+    }else {
+        computerChoice = "SCISSORS";
+    }
+    console.log(`User selected: ${userChoice}`);
+    console.log(`Computer selected: ${computerChoice}`);
+
+    //Determine winner
+    if (userChoice === computerChoice){
+        console.log("It is a Tie. Try again!");
+    }else if ((userChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (userChoice === "PAPER" && computerChoice === "ROCK") ||
+    (userChoice === "SCISSORS" && comp[userChoice === "PAPER"])
+    ){
+        console.log("User wins!")
+    }else{
+        console.log("Computer wins!");
+    }
+
+});
